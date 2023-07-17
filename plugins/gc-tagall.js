3d0d404ae011cc62a46484678f2f77b1932f3711
GeralBot <133059729+GeralBot@users.noreply.github.com>
2023-07-16 21:53:58 -0500

    a

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -8,9 +8,8 @@ let oi = `@𝐢𝐭𝐬.𝐛𝐫𝐮𝐧𝐢𝐭𝐨𝐩: ${pesan}`
 
 let teks = `𝐃𝐞𝐬𝐩𝐢𝐞𝐫𝐭𝐞𝐧 𝐏𝐥𝐚𝐧𝐭𝐢𝐭𝐚𝐬 🌿 \n\n❏ ${oi}\n❏ 𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚𝐬: \n`
 for (let mem of participants) {
-teks += 
-
-`🦀🧿 @${mem.id.split('@')[0]}\n`}
+teks += `
+🦀🧿@${mem.id.split('@')[0]}\n`}
 teks += `
 ➥ 𝐁𝐫𝐮𝐧𝐢𝐭𝐨𝐗𝐓𝐚𝐧𝐢𝐚`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
