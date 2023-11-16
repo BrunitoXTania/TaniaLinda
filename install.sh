54950090e5c0e7511621ad96738cb529f8a94cb0
BrunitoXTania <145606165+BrunitoXTania@users.noreply.github.com>
2023-11-16 16:29:42 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -160,12 +160,11 @@ echo -e "\e[35m
 ─────────────────────────────────────────────────────────\n\e[0m"
 
 echo -e "\033[1;35m"
-git clone https://github.com/OFC-YOVANI/HADES-BOT-OMEGA.git
+git clone https://github.com/BrunitoXTania/TaniaLinda.git
 echo -e "\033[01;32m\033[01mLa clonación se ha descargado e instalado correctamente.\nThe clone has been downloaded and installed successfully.\n\033[0m"
 
 echo -e "\033[01;32m\033[01mCambiando al directorio del repositorio!!\nChanging to the repository directory!!\n\033[0m" 
-cd HADES-BOT-OMEGA
-
+cd TaniaLinda
 
 echo -e "\e[36m
 ██╗███╗░░██╗░██████╗████████╗░█████╗░██╗░░░░░██╗░░░░░  ███╗░░██╗██████╗░███╗░░░███╗
